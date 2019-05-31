bc64c492523fe442861bfbb729ec02df1645a473
Ryota Ikezawa <pavegy@gmail.com>
2019-06-01 00:47:40 +0900

    Detail configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import { ReactNode } from "react";
 
 class App extends React.Component {
   public render(): ReactNode {
+    let name = App.appName("App");
     return (
       <div className="App">
-        <h1>App!</h1>
+        <h1>{name}</h1>
       </div>
     );
+  }
+
+  private static appName(name: string): string {
+    return name + "!";
   }
 }
 

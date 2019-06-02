cca6a03a6ded4e330538074877232aa6a4b86ce3
Ryota Ikezawa <pavegy@gmail.com>
2019-06-02 16:42:19 +0900

    Improve printWidth

--- src/redux/modules/configureStore.tsx
+++ src/redux/modules/configureStore.tsx
@@ -6,9 +6,5 @@ import createRootReducer from "./reducer";
 export const history = createBrowserHistory();
 
 export default function configureStore(preloadedState: any) {
-  return createStore(
-    createRootReducer(history),
-    preloadedState,
-    compose(applyMiddleware(routerMiddleware(history)))
-  );
+  return createStore(createRootReducer(history), preloadedState, compose(applyMiddleware(routerMiddleware(history))));
 }
